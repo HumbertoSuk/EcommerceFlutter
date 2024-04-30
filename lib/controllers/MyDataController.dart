42bf6192cff1ff83bc8b8f972a69e441b6e3faff
unknown <kevin>
2024-04-30 14:09:15 -0700

    agregue otro ejemplo en comentario en el controlador del crud

--- lib/controllers/MyDataController.dart
+++ lib/controllers/MyDataController.dart
@@ -22,3 +22,33 @@ class MyDataController {
     await _dataCollection.doc(id).delete();
   }
 }
+
+/*
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tu_app/model/my_data_model.dart';
+
+class MyDataController {
+  final CollectionReference _dataCollection = FirebaseFirestore.instance.collection('myData');
+
+  // Método para agregar un nuevo documento a la colección
+  Future<void> addData(MyDataModel newData) async {
+    await _dataCollection.add(newData.toMap());
+  }
+
+  // Método para obtener todos los documentos de la colección
+  Stream<List<MyDataModel>> getData() {
+    return _dataCollection.snapshots().map((snapshot) => snapshot.docs.map((doc) => MyDataModel.fromFirestore(doc)).toList());
+  }
+
+  // Método para actualizar un documento existente en la colección
+  Future<void> updateData(String id, Map<String, dynamic> newData) async {
+    await _dataCollection.doc(id).update(newData);
+  }
+
+  // Método para eliminar un documento de la colección
+  Future<void> deleteData(String id) async {
+    await _dataCollection.doc(id).delete();
+  }
+}
+
+*/
